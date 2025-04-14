10cac687d70bfb4c36bf915231a581aaf0ef2f91
Woker-Xd <sreejanxd1@gmail.com>
2025-04-14 13:49:49 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*🔥 Ｒａｂｂｉｔ-MD SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*🔥 Ｒａｂｂｉｔ- SPEED : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
